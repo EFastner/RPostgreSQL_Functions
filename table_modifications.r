fd993d37fba37f8619fbd2b68615e879dcc4ab29
EFastner <eric.fastner@gmail.com>
2019-01-19 18:47:05 -0600

    Changed fun.create_table to accept a variable for table names and column names/types. Added function to create a string value for creating column names in SQL

--- table_modifications.r
+++ table_modifications.r
@@ -1,18 +1,32 @@
-fun.create_table <- function(connection){
+fun.create_table <- function(connection, table_name, column_names){
   #DESCRIPTION: Sends a CREATE TABLE script to PostGres Connection
   #Arguments: connection = a PostgreSQL connection created with fun.postgres_connect
-  #Edit the sql_command variable as needed to create all needed columns
+  #           table_name = a string variable naming the table
+  #           column_names = a string variable naming the columns and defining their types
   
   require(RPostgreSQL)
   
-  sql_command <- "CREATE TABLE lock_line
-(
-  team_game CHAR (2),
-  lock_line CHAR (3),
-  no_return CHAR (3)
-);"
-  
+  sql_command <- paste("CREATE TABLE ", table_name, " (", column_names, ");")
+
   dbGetQuery(connection, sql_command)  
+}
+
+fun.get_row_names <- function(df, col_type = "VARCHAR"){
+  #DESCRIPTION: Outputs a string character to create a list of column names in SQL Database based on the existing data table's column names
+  #ARGUMENTS: df = a dataframe with desired column names in the first column and optionally data types in second
+  #           col_type = denotes the type of column to create if not supplied in df
+  #
+  #WILL NOT FIX COLUMNS WITH INVALID SQL CHARACTERS. YOU WILL STILL NEED TO DO THAT YOURSELF
+  
+  if(ncol(df) == 1){
+    cols <- paste(colnames(gmorning), collapse = paste0(" ", col_type, ", "))
+    cols <- paste(cols, " ", col_type)
+  }else{
+   cols <- paste(df[,1], df[,2])
+   cols <- paste(cols, collapse = ", ")
+  }
+  
+  return(cols)
 }
 
 fun.append_table <- function(connection, table, values){
